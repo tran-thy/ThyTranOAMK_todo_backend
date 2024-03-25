61ada8c4fdb3ddae90735be6e62994019001ab34
thy.tran <kathytran11@gmail.com>
2024-03-25 17:05:02 +0200

    env.vault added

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const query = async (sql, values = []) => {
+const query = async(sql, values = []) => {
     try {
         const pool = openDb();
         const result = await pool.query(sql, values);
@@ -12,14 +12,15 @@ const query = async (sql, values = []) => {
 };
 
 const openDb = () => {
-  const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
-  return pool;
+    const pool = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        ssl: process.env.SSL
+    });
+    return pool;
 };
 
 module.exports = {
