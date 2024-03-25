a725b9e839a7310de6cb61fee5e1653d15bdaf1c
thy.tran <kathytran11@gmail.com>
2024-03-25 19:46:27 +0200

    Modified const port

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', todoRouter);
 
-// const port = process.env.PORT;
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
+// const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
